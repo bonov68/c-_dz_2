eda0faa73012fc56e11b024a11c58a67bb25bb8c
bonov68 <example@mail.ru>
2022-12-04 11:01:53 +0300

    task 10 done

---
+++ task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу ,кот. на вход принимает тоехзначное  число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine ("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int
+ b = a % 100;
+int c = b / 10;  
+Console.WriteLine(c);
